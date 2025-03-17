248e3e6088c7fe3f2020d705ae35e983fa7818ff
Neh Joshi <nehjoshi5@gmail.com>
2025-03-17 12:01:16 -0400

    Genesis

---
+++ src/app/fonts.ts
@@ -0,0 +1,11 @@
+import { Open_Sans, Montserrat } from "next/font/google";
+
+export const openSans = Open_Sans({
+    variable: "--font-open-sans",
+    subsets: ["latin"]
+})
+
+export const montserrat = Montserrat({
+    variable: "--font-montserrat",
+    subsets: ["latin"]
+})
